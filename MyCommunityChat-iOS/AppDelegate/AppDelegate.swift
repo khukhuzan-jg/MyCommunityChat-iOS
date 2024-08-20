510c6fc28eafe3fba0829f6b0b44c66ea2b160aa
kukuzan <khukhuzan@rezeve.com>
2024-08-20 15:50:39 +0700

    added: project scheme manage

--- MyCommunityChat-iOS/AppDelegate/AppDelegate.swift
+++ MyCommunityChat-iOS/AppDelegate/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let baseURL = URL(string: Bundle.main.infoDictionary?["BASE_URL"] as? String ?? "")!
+        print("BaseURL===>",baseURL)
         return true
     }
 
